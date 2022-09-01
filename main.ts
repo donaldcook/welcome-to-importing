604c19385e6365346e04c7046fd71cc6e0778fc5
donaldcook <donald.h.cook@gmail.com>
2022-09-01 22:49:05 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("You have just")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("imported a program.")
+})
+basic.showString("Hello Students")
+basic.forever(function () {
+	
+})
